60ec4f167eb9c8ffe7ab302564c30b058825317b
MasterVisput <55348522+MasterVisput@users.noreply.github.com>
2020-03-28 19:03:09 +0300

    Добавлены файлы: конфиг, иниц. тест.

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default = 'en',
+                     help='Choose language: ru or en')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    if user_language != None:
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError('--language should be specified')
+    yield browser
+    browser.quit()
+
